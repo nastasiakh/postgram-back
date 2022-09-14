ee9162393ac3a10b79dffad19fd0df86cac61a85
kasanst <nastiahlueva@gmail.com>
2022-09-14 15:04:28 +0300

    Added failed version of port

--- src/repositories/posts_repository.js
+++ src/repositories/posts_repository.js
@@ -17,27 +17,30 @@ class PostsRepository{
     }
     async getOne(id){
         const db = pgp(connection)
-        const record = await db.one('SELECT id, title, author FROM posts WHERE id=$1', id)
+        const record = await db.one('SELECT id, title, author FROM posts WHERE id=$1', id);
         return record;
     }
     async create(post){
         const db = pgp(connection)
-        const record = await db.one('INSERT INTO posts (title, author) VALUES ($1, $2) RETURNING id, title, author',
-            [post.title, post.author ])
+        const record = await db.one(
+            'INSERT INTO posts (title, author) VALUES ($1, $2) RETURNING id, title, author',
+            [post.title, post.author]);
         return record;
     }
-    async replaceOne(post){
-        const db = pgp(connection)
-        const record = await db.one('UPDATE posts SET title=$2, author=$3 WHERE id=$1 RETURNING id, title, author',
-           [post.id, post.title, post.author ])
-        return record;
-    }
-    async updateOne(id, post){
-        const db = pgp(connection)
-        const record = await db.one('UPDATE posts SET title=$2, author=$3 WHERE id=$1 RETURNING id, title, author',
-            [id, post.title, post.author ])
-        return record;
-    }
+    // async replaceOne(post){
+    //     const db = pgp(connection)
+    //     const record = await db.one(
+    //         'UPDATE posts SET title=$2, author=$3 WHERE id=$1 RETURNING id',
+    //        [post.id, post.title, post.author ])
+    //     return record;
+    // }
+    // async updateOne(id, post){
+    //     const db = pgp(connection)
+    //     const record = await db.one(
+    //         'UPDATE posts SET title=$2, author=$3 WHERE id=$1 RETURNING (id, title, author)',
+    //         [id, post.title, post.author])
+    //     return record;
+    // }
     async deleteOne(id){
         const db = pgp(connection)
         const record = await db.one('DELETE id, title, author FROM posts WHERE id=$1 RETURNING id, title, author', id)
